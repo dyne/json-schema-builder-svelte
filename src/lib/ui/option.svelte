b3276c0f60dc2ee814eccea22d7f4aed108437c9
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-19 18:58:27 +0100

    removed ui components

--- src/lib/ui/option.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let value: any;
-	export let disabled = false;
-	export let placeholder = '';
-</script>
-
-<option {disabled} {placeholder} class="x-option" {value}>
-	<slot />
-</option>
