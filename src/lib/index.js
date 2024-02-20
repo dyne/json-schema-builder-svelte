c3ff41bf1e14fac931864a878b77054f511545c8
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-20 12:34:14 +0100

    feat: update lib exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,6 @@
 import JSONSchemaBuilder from './JSONSchemaBuilder/JSONSchemaBuilder.svelte';
 import JSONSchemaField from './JSONSchemaField/JSONSchemaField.svelte';
+import JSONSchemaEditor from './JSONSchemaEditor/JSONSchemaEditor.svelte';
+import { setStrings } from './ui/strings.js';
 
-export { JSONSchemaBuilder, JSONSchemaField };
+export { JSONSchemaBuilder, JSONSchemaField, JSONSchemaEditor, setStrings };
