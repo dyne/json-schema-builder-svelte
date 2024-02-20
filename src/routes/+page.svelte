f0fe53d61faae5d72a8829df69b9a44854999e48
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-20 12:40:30 +0100

    feat: completed support for strings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import JSONSchemaEditor from '$lib/JSONSchemaEditor/JSONSchemaEditor.svelte';
+	import { setStrings } from '$lib/ui/strings.js';
 
 	let schema = '{"type": "object", "properties": {"ok":{"type":"array"}}}';
+
+	// setStrings({ view_in_plain_text: 'Ciao' });
 </script>
 
 <div class="space-y-4 p-4">
