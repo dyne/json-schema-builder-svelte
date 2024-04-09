7f1881f3a850bd3ff9dc80a02ed261f8224ca294
puria <puria@dyne.org>
2024-04-10 01:47:57 +0200

    build: add dist builds

--- dist/logic/validation.d.ts
+++ dist/logic/validation.d.ts
@@ -1,6 +1,6 @@
 import { Effect } from 'effect';
+import { BaseError, ErrorCode } from './errors.js';
 import { type JSONSchema, type Property } from './types.js';
-import { ErrorCode, BaseError } from './errors.js';
 export declare const validatePropertyListKeys: (propertyList: Property[]) => Effect.Effect<Property[], DuplicateKeysError, never>;
 declare class DuplicateKeysError extends BaseError {
     readonly _tag = ErrorCode.DuplicateKeysError;
