b8cda53e3e05ae4ec92613fd5c7ea7df38bd0fab
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-06-06 21:52:15 +0200

    test: page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	import JSONSchemaEditor from '$lib/JSONSchemaEditor/JSONSchemaEditor.svelte';
-	import { debugSchema } from '$lib/logic/utils.js';
+	import type { JSONObjectSchema } from '$lib/logic/types.js';
+	import { createJSONObjectSchema, debugSchema } from '$lib/logic/utils.js';
 	// import { setStrings } from '$lib/ui/strings.js';
 
-	let schema = '{"type": "object", "properties": {"ok":{"type":"string"}}}';
+	let schema = createJSONObjectSchema();
 
+	const schema_1: JSONObjectSchema = {
+		type: 'object',
+		properties: { ok: { type: 'string' }, no: { type: 'integer' } }
+	};
+
+	function setSchema() {
+		schema = schema_1;
+	}
 	// setStrings({ view_in_plain_text: 'Ciao' });
 </script>
+
+<button class="x-button" on:click={setSchema}>set</button>
 
 <div class="space-y-4 p-4">
 	<JSONSchemaEditor bind:schema mode="builder" requiredDefault hideRequired />
